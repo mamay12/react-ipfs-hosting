1acdbe192ca6c483a93c6d8a361369d841e71b1a
Yaroslav Tymchenko <ytimchenko57@gmail.com>
2025-05-14 13:55:10 +0300

    add new steps in actions

--- .github/pinata-upload.js
+++ .github/pinata-upload.js
@@ -2,10 +2,12 @@ import pinataSDK from '@pinata/sdk';
 import {fileURLToPath} from 'url';
 import {dirname} from 'path';
 import path from 'path';
+import fs from 'fs';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-const distPath = path.join(__dirname, '..', 'dist');
+const rootPath = path.join(__dirname, '..');
+const distPath = path.join(rootPath, 'dist');
 
 const pinataJWT = process.env.PINATA_JWT;
 
@@ -26,6 +28,12 @@ try {
     });
 
     console.log('✅ Success:', result);
+    const cid = result.IpfsHash;
+
+    console.log(cid);
+
+    const link = `https://${cid}.ipfs.dweb.link/`;
+    fs.writeFileSync(path.join(rootPath, 'link.txt'), link);
 } catch (err) {
     console.error('❌ Error:', err);
 }
